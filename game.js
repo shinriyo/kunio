9d98bc3b1431e18e9eb256c16fccb197c35610c1
shinriyo <shinriyo@gmail.com>
2012-05-16 08:48:22 +0900

    ok

--- game.js
+++ game.js
@@ -5,69 +5,95 @@ window.onload = function() {
     game.fps = 24;
     game.preload('chara1.png');
     var score = 0;
-    var timeLeft = 10 * game.fps;
+    var timeLeft = 60 * game.fps; // 60minuits
 
     game.onload = function() {
         game.keybind(90, 'a'); //z
         game.keybind(88, 'b'); //x
         //game.keybind(67, 'c'); //c
         var bear = new Sprite(32, 32);
+        bear.current_y = 0;
         bear.frame = 1;
         bear.image = game.assets['chara1.png'];
         //Math.floor(Math.random() * 300) + 10;
         bear.x = 10;
-        bear.y = 10;
+        bear.y = 30;
         bear.vy = 0;
         bear.jumping = false;
+        bear.speed = 5;
 
         var bear2 = new Sprite(32, 32);
+        bear2.current_y = 0;
         bear2.frame = 2;
-        bear.image = game.assets['chara1.png'];
+        bear2.x = 60;
+        bear2.y = 30;
+        bear2.image = game.assets['chara1.png'];
         var chara_speed = {'bear':5};
         var timeLabel = new Label('Time 0');
         timeLabel.x = 5;
         timeLabel.y = 5;
 
+        var label = new Label('message');
+        label.x = 55;
+        label.y = 5;
+        label.color = 'red';
+        label.font = "bold 24px 'Impact'";
+
         var scoreLabel = new Label('Score: 0');
         scoreLabel.x = 200;
         scoreLabel.y = 5;
-        var bear_current_y = 0;
+        var player1Label = new Label("player1: hoge");
+        player1Label.x = 5;
+        player1Label.y = 200;
+        var player2Label = new Label("player2: bar");
+        player2Label.x = 80;
+        player2Label.y = 200;
+        var player3Label = new Label("player3: huga");
+        player3Label.x = 155;
+        player3Label.y = 200;
+        var player4Label = new Label("player4: boo");
+        player4Label.x = 230;
+        player4Label.y = 200;
+
         bear.addEventListener('enterframe', function() {
+            if (this.within(bear2, 5)) {
+                label.text = 'hit';
+            }
+
             if (game.input.left) {
-               this.x -= chara_speed['bear'];
+               this.x -= this.speed;
             }
             if (game.input.right) {
-               this.x += chara_speed['bear'];
+               this.x += this.speed;
             }
-            if (!bear.jumping) {
+            if (!this.jumping) {
                 if (game.input.up) {
-                   this.y -= chara_speed['bear'];
+                   this.y -= this.speed;
                 }
                 if (game.input.down) {
-                   this.y += chara_speed['bear'];
+                   this.y += this.speed;
                 }
                 // jump button
                 if (game.input.a) {
-                    bear_current_y = bear.y;
-                    bear.vy = -9;
-                    //bear.y = -1;
-                    bear.jumping = true;
+                    this.current_y = this.y;
+                    this.vy = -9;
+                    this.jumping = true;
                 }
             }
-            bear.vy += 0.9;
-            if (bear.jumping) bear.y += bear.vy;
+            this.vy += 0.9;
+            if (this.jumping) this.y += this.vy;
             //scoreLabel.text = 'Score: ' + score;
-            if (bear.jumping && bear.y >= bear_current_y) {
-                bear.y = bear_current_y;
-                bear.jumping = false;
+            if (this.jumping && bear.y >= this.current_y) {
+                this.y = this.current_y;
+                this.jumping = false;
             }
         });
 
         game.addEventListener('enterframe', function() {
+            timeLeft--;
             if (timeLeft <= 0) {
                 this.stop();
             }
-            timeLeft--;
             timeLabel.text = 'Time: ' + timeLeft;
         });
 
@@ -75,6 +101,11 @@ window.onload = function() {
         game.rootScene.addChild(bear2);
         game.rootScene.addChild(scoreLabel);
         game.rootScene.addChild(timeLabel);
+        game.rootScene.addChild(label);
+        game.rootScene.addChild(player1Label);
+        game.rootScene.addChild(player2Label);
+        game.rootScene.addChild(player3Label);
+        game.rootScene.addChild(player4Label);
     };
 
     game.start();
